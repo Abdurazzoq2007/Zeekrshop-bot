004a72146ed325ede1d1108afc8042fea4743a91
Abdurazzoq Dilshodov <abduchik1@gmail.com>
2025-08-10 20:47:31 +0500

    NewKode

--- data.js
+++ data.js
@@ -4,14 +4,298 @@ module.exports = {
   products: {
     'Zeekr 001': {
       Aksessuarlar: [
-        { name: 'Stakan ushlagich', description: 'Premium sifatli ushlagich Zeekr 001 uchun.' },
-        { name: 'Kovrik to‘plami', description: '3 qavatli pol qoplamasi.'  }
+        { 
+          fullname:" Zeekr 001",
+          name: 'Oригинальный салон коврик.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/1.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Oригинальный багаж коврик.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/2.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Cалон коврик TPE.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/3.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Коврик багажника TPE.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/4.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Брызговик.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/5.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Органайзер бара.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/6.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Органайзер под бара.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/7.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Накладка на порог из нержавика.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/8.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Накладка на порог из карбона.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/9.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Солнцезащитная шторка.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/10.png'
+        },
+        // yengi
+        { 
+          fullname:" Zeekr 001",
+          name: 'Оригинальная защита сиденья.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/11.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Стекло защита монитора.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/12.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Накладка на карманы 4 дверей.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/13.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Накладка на кнопки 4 дверей.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/14.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Накидка сидений.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/15.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Оригинальная силиконовая накладка консоли.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/16.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Радиаторная сетка.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/17.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Ветровик.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/18.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Защита стойки.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/19.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Защитная обшивка.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/20.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Большой футляр очков.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/21.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Мини-футляр очков.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/22.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Дворник.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/23.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Салонный фильтр.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/24.png'
+        }
+      ],
+ Tuning: [
+        { 
+          fullname:" Zeekr 001",
+          name: 'Кристалл рычаг до рестайлинга.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_1.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Кристалл рычаг рестайлинга.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_2.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Карбоновые детали до рестайлинга.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_3.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'До Рестайлинг body kit.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_4.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'ZEEKR FR спойлер.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_5.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Спойлер.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_6.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Рестайлинг body kit.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_7.png'
+        },
+        { 
+          fullname:" Zeekr 001",
+          name: 'Кнопка руля.', 
+          description: 'Премиум-класс для Zeekr 001.',
+          line:"______________",
+          text:'Магазин «ZeekrShop» предлагает\n широкий ассортимент идеальных\n аксессуаров для электромобилей!',
+          admin:"@zzz770017 , @zeekrshop1",
+          image:'./Zeekr_001/img/Tuning_8.png'
+        }
       ]
-    },
-    'Zeekr 007': {
-      Aksessuarlar: [
-        { name: 'Telefon ushlagich', description: 'Havo ventilyatsiyasiga o‘rnatiladigan.' }
-      ]
-    }
-  }
+    } 
+   }
 };
--- main.js
+++ main.js
@@ -1,89 +1,274 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const { models, categories, products } = require('./data');
+const { models, categories, products } = require('./data');  // data.js ichida to‘g‘ri ma'lumot bo‘lishi kerak
 const { ADMINS } = require('./config');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-// Boshlanish
+const userState = {};  // Foydalanuvchi holatlarini saqlash
+
+// /start komandasi
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    sendModelSelection(chatId);
+  const chatId = msg.chat.id;
+  userState[chatId] = null;
+  sendModelSelection(chatId);
 });
 
 // Model tanlash
 function sendModelSelection(chatId) {
-    bot.sendMessage(chatId, "Qaysi model uchun mahsulot qidiryapsiz?", {
-        reply_markup: {
-            keyboard: [...models.map(m => [m])],
-            resize_keyboard: true
-        }
-    });
+  bot.sendMessage(chatId, "Qaysi model uchun mahsulot qidiryapsiz?", {
+    reply_markup: {
+      keyboard: models.map(m => [m]),
+      resize_keyboard: true
+    }
+  });
 }
 
 // Kategoriya tanlash
 function sendCategorySelection(chatId) {
-    bot.sendMessage(chatId, "Qaysi kategoriya?", {
-        reply_markup: {
+  bot.sendMessage(chatId, "Qaysi kategoriya?", {
+    reply_markup: {
+      keyboard: [...categories.map(c => [c]), ["🔙 Orqaga qaytish"]],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  });
+}
+
+// Mahsulotni inline tugmalar bilan jo'natish
+function sendProductWithInlineButtons(chatId, p, count = 0) {
+  const caption = `🚗 ${p.fullname}\n🛒 ${p.name}\n📄 ${p.description}\n${p.line}\n\n📝${p.text}\n\n🧑🏻‍💻 ${p.admin}\n\nSizning soningiz: ${count}`;
+  bot.sendPhoto(chatId, p.image, {
+    caption,
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "-", callback_data: `decrease|${p.name}` },
+          { text: "Savatcha", callback_data: "show_cart" },
+          { text: "+", callback_data: `increase|${p.name}` }
+        ]
+      ]
+    }
+  });
+}
+// Asosiy xabarlarni qabul qilish
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (msg.contact) return; // Kontakt alohida eventda
+
+  if (!userState[chatId]) {
+    if (models.includes(text)) {
+      userState[chatId] = { step: 'category', model: text, cart: {} };
+      sendCategorySelection(chatId);
+    } else {
+      sendModelSelection(chatId);
+    }
+    return;
+  }
+
+  const state = userState[chatId];
+
+  if (state.step === 'category') {
+    if (text === "🔙 Orqaga qaytish") {
+      userState[chatId] = null;
+      sendModelSelection(chatId);
+      return;
+    }
+
+    if (categories.includes(text)) {
+      state.category = text;
+      state.step = 'showProducts';
+
+      const productList = products?.[state.model]?.[text];
+      if (productList?.length) {
+        // Mahsulotlarni inline tugmalar bilan ko'rsatish
+        productList.forEach(p => {
+          const count = state.cart[p.name] || 0;
+          sendProductWithInlineButtons(chatId, p, count);
+        });
+
+        bot.sendMessage(chatId, "Buyurtma berish uchun kontakt yuboring yoki boshqa kategoriya tanlang:", {
+          reply_markup: {
             keyboard: [
-                ...categories.map(c => [c]),
-                ["🔙 Orqaga qaytish"]
+              [{ text: "📞 Kontaktni yuborish", request_contact: true }],
+              ["🔙 Boshqa kategoriya"]
             ],
             resize_keyboard: true,
             one_time_keyboard: true
-        }
-    });
-}
-
-// Xabarlar
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-
-    if (models.includes(text)) {
-        const model = text;
-        sendCategorySelection(chatId);
-
-        bot.once('message', (catMsg) => {
-            const category = catMsg.text;
-
-            if (category === "🔙 Orqaga qaytish") {
-                sendModelSelection(chatId);
-                return;
-            }
-
-            const productList = products?.[model]?.[category];
-
-            if (productList?.length) {
-                productList.forEach(p => {
-                    bot.sendMessage(chatId, `🛒 ${p.name}\n📄 ${p.description}`);
-                });
-
-                bot.sendMessage(chatId, "Buyurtma berish uchun kontakt yuboring:", {
-                    reply_markup: {
-                        keyboard: [[{
-                            text: "📞 Kontaktni yuborish",
-                            request_contact: true
-                        }]],
-                        resize_keyboard: true,
-                        one_time_keyboard: true
-                    }
-                });
-            } else {
-                bot.sendMessage(chatId, "Bu bo‘limda hali mahsulotlar mavjud emas.");
-            }
-
-            bot.once('contact', (contactMsg) => {
-                const contact = contactMsg.contact;
-                ADMINS.forEach(admin => {
-                    bot.sendMessage(admin, `📞 Yangi buyurtma:
-Ismi: ${contact.first_name}
-Telefon: ${contact.phone_number}
-Model: ${model}
-Kategoriya: ${category}`);
-                });
-                bot.sendMessage(chatId, "Rahmat! Tez orada siz bilan bog‘lanamiz.");
-            });
+          }
         });
+
+      } else {
+        bot.sendMessage(chatId, "Bu bo‘limda hali mahsulotlar mavjud emas.");
+        sendCategorySelection(chatId);
+      }
+      return;
     }
+
+    bot.sendMessage(chatId, "Iltimos, kategoriyani tanlang.");
+    return;
+  }
+
+  if (state.step === 'showProducts') {
+    if (text === "🔙 Boshqa kategoriya") {
+      state.step = 'category';
+      sendCategorySelection(chatId);
+      return;
+    }
+  }
+
+  // Agar foydalanuvchi modelni qayta tanlasa
+  if (models.includes(text)) {
+    userState[chatId] = { step: 'category', model: text, cart: {} };
+    sendCategorySelection(chatId);
+    return;
+  }
+});
+// Inline tugmalar bosilganda ishlov berish
+bot.on('callback_query', (query) => {
+  const chatId = query.message.chat.id;
+  const data = query.data;
+  const state = userState[chatId];
+  if (!state) {
+    bot.answerCallbackQuery(query.id, { text: "Avval model va kategoriya tanlang" });
+    return;
+  }
+
+  if (data.startsWith('increase|')) {
+    const productName = data.split('|')[1];
+    if (!state.cart) state.cart = {};
+    state.cart[productName] = (state.cart[productName] || 0) + 1;
+    bot.answerCallbackQuery(query.id, { text: `${productName} miqdori oshirildi` });
+    updateProductMessage(chatId, query.message.message_id, productName, state);
+    return;
+  }
+
+  if (data.startsWith('decrease|')) {
+    const productName = data.split('|')[1];
+    if (state.cart && state.cart[productName]) {
+      state.cart[productName] = Math.max(0, state.cart[productName] - 1);
+      if (state.cart[productName] === 0) delete state.cart[productName];
+      bot.answerCallbackQuery(query.id, { text: `${productName} miqdori kamaytirildi` });
+      updateProductMessage(chatId, query.message.message_id, productName, state);
+    } else {
+      bot.answerCallbackQuery(query.id, { text: `${productName} savatchada yoq` });
+    }
+    return;
+  }
+
+  if (data === "show_cart") {
+    if (!state.cart || Object.keys(state.cart).length === 0) {
+      bot.sendMessage(chatId, "Savatcha bo‘sh.");
+    } else {
+      let cartText = "Tanlangan mahsulotlar:\n\n";
+      for (const [name, count] of Object.entries(state.cart)) {
+        cartText += `• ${name}: ${count}\n`;
+      }
+      cartText += `\nBuyurtmani yuborish uchun quyidagini bosing:`;
+      bot.sendMessage(chatId, cartText, {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Buyurtmani adminlarga yuborish", callback_data: "send_order" }]
+          ]
+        }
+      });
+    }
+    bot.answerCallbackQuery(query.id);
+    return;
+  }
+
+  if (data === "send_order") {
+    if (!state.cart || Object.keys(state.cart).length === 0) {
+      bot.answerCallbackQuery(query.id, { text: "Savatcha bo‘sh, hech narsa yuborilmadi." });
+      return;
+    }
+    let orderText = "Yangi buyurtma:\n\n";
+    for (const [name, count] of Object.entries(state.cart)) {
+      orderText += `• ${name}: ${count}\n`;
+    }
+    ADMINS.forEach(admin => {
+      bot.sendMessage(admin, orderText);
+    });
+    bot.sendMessage(chatId, "Buyurtmangiz adminlarga yuborildi. Iltimos, kontaktni yuboring.", {
+      reply_markup: {
+        keyboard: [[{
+          text: "📞 Kontaktni yuborish",
+          request_contact: true
+        }]],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      }
+    });
+    userState[chatId].step = 'waiting_contact';
+    bot.answerCallbackQuery(query.id, { text: "Buyurtma yuborildi." });
+    return;
+  }
+});
+function updateProductMessage(chatId, messageId, productName, state) {
+  const productList = products?.[state.model]?.[state.category];
+  if (!productList) return;
+
+  const p = productList.find(prod => prod.name === productName);
+  if (!p) return;
+
+  const count = state.cart[productName] || 0;
+  const caption = `🚗 ${p.fullname}\n🛒 ${p.name}\n📄 ${p.description}\n${p.line}\n\n📝${p.text}\n\n🧑🏻‍💻 ${p.admin}\n\nSizning soningiz: ${count}`;
+
+  bot.editMessageCaption(caption, {
+    chat_id: chatId,
+    message_id: messageId,
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "-", callback_data: `decrease|${p.name}` },
+          { text: "Savatcha", callback_data: "show_cart" },
+          { text: "+", callback_data: `increase|${p.name}` }
+        ]
+      ]
+    }
+  }).catch(() => {
+    // Ba'zida xatolik bo'lishi mumkin, masalan xabar o'zgartirilganda
+  });
+}
+// Kontakt qabul qilish
+bot.on('contact', (msg) => {
+  const chatId = msg.chat.id;
+  const contact = msg.contact;
+  const state = userState[chatId];
+
+  if (!state || state.step !== 'waiting_contact') {
+    bot.sendMessage(chatId, "Iltimos, avval buyurtma bering.");
+    return;
+  }
+
+  ADMINS.forEach(admin => {
+    bot.sendMessage(admin, `📞 Kontakt ma’lumotlari:
+Ismi: ${contact.first_name}
+Telefon: ${contact.phone_number}`);
+  });
+
+  bot.sendMessage(chatId, "Rahmat! Tez orada siz bilan bog‘lanamiz.", {
+    reply_markup: {
+      keyboard: [['Bosh menyu']],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  });
+
+  userState[chatId] = null; // holatni tozalash
+});
+
+// Asosiy menyuga qaytish tugmasi bosilganda
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === 'Bosh menyu') {
+    sendModelSelection(chatId);
+    userState[chatId] = null; // agar kerak bo‘lsa holatni tozalash
+  }
+
+  // ... qolgan xabarlarni qabul qilish kodi shu yerda davom etadi ...
 });
